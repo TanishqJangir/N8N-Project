5bfa41df2133ac2ca20a987dfbad2852c5707578
TanishqJangir <tanishqjangir001@gmail.com>
2026-02-07 19:52:52 +0530

    06: background jobs

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,5 +1,3 @@
-import Link from "next/link";
-import Image from "next/image";
 import { AuthLayout } from "@/features/auth/components/auth-layout";
 
 
--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,10 +1,22 @@
+import { inngest } from "@/inngest/client";
 import { createTRPCRouter, protectedProcedure } from "../init";
 import prisma from "@/lib/db";
 
 export const appRouter = createTRPCRouter({
-    getUsers: protectedProcedure.query(({ctx}) => {
-        return prisma.user.findMany();
+    getWorkflows: protectedProcedure.query(({ctx}) => {
+        return prisma.workFlow.findMany();
     }),
+    createWorkflow: protectedProcedure.mutation( async () =>{
+
+        await inngest.send({
+            name : "test/hello.world",
+            data : {
+                email : "tanishqjangir@gmail.com"
+            }
+        })
+
+        return {success : true, message : "Job queued"}
+    })
 });
 
 export type AppRouter = typeof appRouter;
