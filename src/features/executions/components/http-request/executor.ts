884c53c407b4ccc550e8ce5a8ba70c4fb1c15b42
TanishqJangir <tanishqjangir001@gmail.com>
2026-02-20 20:28:47 +0530

    21: node realtime

--- src/features/executions/components/http-request/executor.ts
+++ src/features/executions/components/http-request/executor.ts
@@ -2,12 +2,13 @@ import Handlebars from "handlebars"
 import type { NodeExecutor } from "@/features/executions/types";
 import { NonRetriableError } from "inngest";
 import ky, { type Options as KyOptions } from "ky";
+import { httpRequestChannel } from "@/inngest/channels/http-request";
 
 Handlebars.registerHelper("json", (context) => {
   const jsonString = JSON.stringify(context);
   const safeString = new Handlebars.SafeString(jsonString);
   return safeString;
-});  
+});
 
 
 
@@ -23,68 +24,106 @@ export const httpRequestExecutor: NodeExecutor<HttpRequestData> = async ({
   data,
   nodeId,
   context,
-  step }) => {
+  step,
+  publish,
+}) => {
 
-  //TODO : Publish "loading" state for manual trigger
+  await publish(
+    httpRequestChannel().status({
+      nodeId,
+      status: "loading",
+    })
+  )
 
   if (!data.endpoint) {
-    //TODO: Publish "error" state for http request node
+    await publish(
+      httpRequestChannel().status({
+        nodeId,
+        status: "error",
+      })
+    )
     throw new NonRetriableError("HTTP Request node: No endpoint configured");
 
   }
 
   if (!data.variableName) {
-    //TODO: Publish "error" state for http request node
+    await publish(
+      httpRequestChannel().status({
+        nodeId,
+        status: "error",
+      })
+    )
     throw new NonRetriableError("Variable name not configured");
 
   }
 
   if (!data.method) {
-    //TODO: Publish "error" state for http request node
+    await publish(
+      httpRequestChannel().status({
+        nodeId,
+        status: "error",
+      }),
+    );
     throw new NonRetriableError("Method not configured");
 
   }
 
-  const result = await step.run("http-request", async () => {
-    const endpoint = Handlebars.compile(data.endpoint)(context);
-    const method = data.method;
 
-    const options: KyOptions = { method };
+  try {
+    const result = await step.run("http-request", async () => {
+      const endpoint = Handlebars.compile(data.endpoint)(context);
+      const method = data.method;
 
-    if (["POST", "PUT", "PATCH"].includes(method)) {
-      const resolved = Handlebars.compile(data.body?.trim() || "{}")(context);
-      JSON.parse(resolved); // Validate JSON
-      options.body = resolved;
-      options.headers = {
-        "Content-Type": "application/json",
+      const options: KyOptions = { method };
+
+      if (["POST", "PUT", "PATCH"].includes(method)) {
+        const resolved = Handlebars.compile(data.body?.trim() || "{}")(context);
+        JSON.parse(resolved); // Validate JSON
+        options.body = resolved;
+        options.headers = {
+          "Content-Type": "application/json",
+        }
       }
-    }
 
-    const response = await ky(endpoint, options);
-    const contentType = response.headers.get("content-type");
-    const responseData = contentType?.includes("application/json")
-      ? await response.json()
-      : await response.text();
+      const response = await ky(endpoint, options);
+      const contentType = response.headers.get("content-type");
+      const responseData = contentType?.includes("application/json")
+        ? await response.json()
+        : await response.text();
 
 
-    const responsePayload = {
-      httpResponse: {
-        status: response.status,
-        statusText: response.statusText,
-        data: responseData,
+      const responsePayload = {
+        httpResponse: {
+          status: response.status,
+          statusText: response.statusText,
+          data: responseData,
+        }
       }
-    }
 
 
-    return {
-      ...context,
-      [data.variableName]: responsePayload
-    }
+      return {
+        ...context,
+        [data.variableName]: responsePayload
+      }
 
 
-  });
+    });
 
-  //TODO : Publish "success" state for manual trigger
+    await publish(
+      httpRequestChannel().status({
+        nodeId,
+        status: "success",
+      }),
+    );
 
-  return result;
+    return result;
+  } catch (error) {
+    await publish(
+      httpRequestChannel().status({
+        nodeId,
+        status: "error",
+      }),
+    );
+    throw error;
+  }
 }
--- src/features/executions/lib/executor-registry.ts
+++ src/features/executions/lib/executor-registry.ts
@@ -3,18 +3,18 @@ import { NodeExecutor } from "../types";
 import { manualTriggerExecutor } from "@/features/triggers/components/manual-trigger/executor";
 import { httpRequestExecutor } from "../components/http-request/executor";
 
-export const executorRegistry : Record<NodeType, NodeExecutor> = {
-    [NodeType.INITIAL] : manualTriggerExecutor,
-    [NodeType.MANUAL_TRIGGER] : manualTriggerExecutor,
-    [NodeType.HTTP_REQUEST] : httpRequestExecutor,
+export const executorRegistry: Record<NodeType, NodeExecutor> = {
+    [NodeType.INITIAL]: manualTriggerExecutor,
+    [NodeType.MANUAL_TRIGGER]: manualTriggerExecutor,
+    [NodeType.HTTP_REQUEST]: httpRequestExecutor, //TODO : Fix types
 }
 
-export const getExecutor = (type : NodeType) : NodeExecutor => {
+export const getExecutor = (type: NodeType): NodeExecutor => {
     const executor = executorRegistry[type];
-    if(!executor){
+    if (!executor) {
         throw new Error(`No executor found for node  type: ${type}`);
 
     }
 
-    return executor;   
+    return executor;
 }
